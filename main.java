92f45506d1fc80750765424106570f6a1d1fad96
wuliow <123999794+wuliow@users.noreply.github.com>
2023-03-05 22:18:28 +0300

    Update main.java

--- main.java
+++ main.java
@@ -3,40 +3,37 @@ import java.util.Scanner;
 public class Calculator {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-
         System.out.println("Введите первое число:");
         double num1 = getNumber(input);
-
         System.out.println("Введите операцию (+, -, *, /):");
         String operation = input.next();
-
         System.out.println("Введите второе число:");
         double num2 = getNumber(input);
 
         double result = 0;
         boolean error = false;
         switch (operation) {
-            case "+" -> result = num1 + num2;
-            case "-" -> result = num1 - num2;
-            case "*" -> result = num1 * num2;
-            case "/" -> {
+            case "+":
+                result = num1 + num2;
+                break;
+            case "-":
+                result = num1 - num2;
+                break;
+            case "*":
+                result = num1 * num2;
+                break;
+            case "/":
                 if (num2 == 0) {
                     System.out.println("Ошибка: деление на 0 невозможно");
                     error = true;
-                }
-                else {
+                } else {
                     result = num1 / num2;
-
                 }
-            }
-
-            default -> {
+                break;
+            default:
                 System.out.println("Ошибка: некорректная операция");
                 error = true;
-
-            }
         }
-
         if (!error) {
             System.out.println("Результат: " + result);
         }
@@ -47,7 +44,6 @@ public class Calculator {
             System.out.println("Ошибка: введите число");
             input.next();
         }
-
         return input.nextDouble();
     }
 }
